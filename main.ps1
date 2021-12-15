b381c7b12a068921607ab5a5fa95ebd50cbcc078
Victor Reipur <victor.reipur@gmail.com>
2021-12-15 23:45:31 +0100

    Explaned

--- main.ps1
+++ main.ps1
@@ -3,7 +3,21 @@ Import-Module "$PSScriptRoot\Install-PipModule.psm1"
 Import-Module "$PSScriptRoot\Install-WheelsPackages.psm1"
 Import-Module "$PSScriptRoot\Install-Python386.psm1"
 
-Install-Python386 # Requires admin rights
+
+
+
+
+
+# Requires admin rights
+Install-Python386 
+
+# This should be commented out if the script runs with admin rights
+# Because no packages will be installed here - I THINK
 Set-EnviromentVariable -NewEnviromentVariable "$env:USERPROFILE\AppData\Roaming\Python\Python38\Scripts"
+
+# Install newest pip module
 Install-PipModule
+
+# If running with admin rights. Then pkgs will be installed C:\Program Files\Python38\Lib\site-packages
+# Otherwise in AppData\Roaming\Python\Python38\Scripts
 Install-WheelsPackages
